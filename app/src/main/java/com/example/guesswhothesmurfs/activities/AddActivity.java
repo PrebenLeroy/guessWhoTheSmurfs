62d16b5ce335e1b8435528b9e9ee257934014179
PrebenLeroy <preben.leroy@live.be>
2018-01-11 22:16:30 +0100

    add multiple screen work

---
+++ app/src/main/java/com/example/guesswhothesmurfs/activities/AddActivity.java
@@ -0,0 +1,18 @@
+package com.example.guesswhothesmurfs.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.guesswhothesmurfs.R;
+
+/**
+ * Created by prebe on 11/01/2018.
+ */
+
+public class AddActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+    }
+}
---
+++ app/src/main/java/com/example/guesswhothesmurfs/activities/StartUpActivity.java
@@ -0,0 +1,33 @@
+package com.example.guesswhothesmurfs.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.guesswhothesmurfs.R;
+
+/**
+ * Created by prebe on 11/01/2018.
+ */
+
+public class StartUpActivity extends AppCompatActivity {
+
+    private Button btnAdd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+
+        this.btnAdd = (Button) findViewById(R.id.btnAdd);
+        this.btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartUpActivity.this, AddActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
