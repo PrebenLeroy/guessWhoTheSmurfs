e1a37ee8a741378c37e55552167117bcb76ccdb3
PrebenLeroy <preben.leroy@live.be>
2018-01-11 22:32:42 +0100

    go back on cancel

--- app/src/main/java/com/example/guesswhothesmurfs/activities/AddActivity.java
+++ app/src/main/java/com/example/guesswhothesmurfs/activities/AddActivity.java
@@ -2,6 +2,9 @@ package com.example.guesswhothesmurfs.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.guesswhothesmurfs.R;
 
@@ -10,9 +13,23 @@ import com.example.guesswhothesmurfs.R;
  */
 
 public class AddActivity extends AppCompatActivity {
+
+    private Button btnCancel;
+    private Button btnAdd;
+    private EditText name;
+    private EditText description;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add);
+
+        this.btnCancel = (Button) findViewById(R.id.button2);
+        this.btnCancel.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                AddActivity.super.finish();
+            }
+        });
     }
 }
